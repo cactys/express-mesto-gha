fe0648f1eea742f9b9e74d8c5562c2c47ad7354a
cactys95 <cactys95@yandex.ru>
2022-09-01 23:37:46 +0300

    feat: add files and schames (user && card)

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ models/card.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    require: true,
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    require: true,
+    default: undefined,
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
