ffe36984611d02ef3436d98645e206dbd120df33
cactys95 <cactys95@yandex.ru>
2022-09-05 12:01:35 +0300

    fix: first review

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const path = require('path');
+// const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
