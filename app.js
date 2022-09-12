d7462dc65311800e1f6f8a0462af926932fe8da6
cactys95 <cactys95@yandex.ru>
2022-09-13 00:23:51 +0300

    feat: что-то поправил, что-то добавил. Не знаю как так вышло!

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const users = require('./routes/users');
 const cards = require('./routes/cards');
 const { ERROR_404 } = require('./utils/code');
 
-// Без файла .env, ошибка в тесте =)
 const { PORT = 3000 } = process.env;
 const app = express();
 
@@ -30,6 +29,4 @@ app.use((req, res) => {
   res.status(ERROR_404).send({ message: 'Страница не найдена' });
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT);
