235b70ef5c58608b644ca1972ec9c6d257d9fff3
cactys95 <cactys95@yandex.ru>
2022-09-20 22:29:23 +0300

    fix: [+] [POST] Добавление пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -55,10 +55,7 @@ module.exports.createUser = (req, res, next) => {
     .then((user) => {
       res
         .status(CODE_201)
-        .send({
-          _id: user._id,
-          email: user.email,
-        });
+        .send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
