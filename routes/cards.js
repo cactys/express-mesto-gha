021e032ceae8d0deb5d3774ee3afbe4d1f856ff6
cactys95 <cactys95@yandex.ru>
2022-09-21 01:46:08 +0300

    fix: [+] [DELETE] Удаление карточки

--- routes/cards.js
+++ routes/cards.js
@@ -13,26 +13,26 @@ router.get('/', getCards);
 
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    link: Joi.string().uri().regex(patternUrl).required(),
+    name: Joi.string().min(2).max(30),
+    link: Joi.string().uri().regex(patternUrl),
   }),
 }), createCard);
 
 router.delete('/:cardId', celebrate({
   body: Joi.object().keys({
-    cardId: Joi.string().hex().length(24).required(),
+    cardId: Joi.string().hex(),
   }),
 }), deleteCard);
 
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
+    cardId: Joi.string().hex(),
   }),
 }), likeCard);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
+    cardId: Joi.string().hex(),
   }),
 }), dislikeCard);
 
