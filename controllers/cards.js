a46d229d0da644433a800da4b3503390500755b4
cactys95 <cactys95@yandex.ru>
2022-09-21 10:02:07 +0300

    fix:

--- controllers/cards.js
+++ controllers/cards.js
@@ -37,9 +37,19 @@ module.exports.deleteCard = (req, res, next) => {
       if (card.owner !== ownerId) {
         throw new ForbiddenError();
       }
-      return res.status(CODE_200).send({ data: card, message: 'DELETE' });
+      // return res.status(CODE_200).send({ data: card, message: 'DELETE' });
+      return card;
     })
+    .then((card) => Card.deleteOne(card))
+    .then((card) => res.send({ data: card, message: 'DELETE' }))
     .catch(next);
+  // .catch((err) => {
+  //   if (err.name === 'CastError') {
+  //     next(new BadRequestError('Картачка не найдена'));
+  //     return;
+  //   }
+  //   next(err);
+  // });
 };
 
 module.exports.likeCard = (req, res, next) => {
