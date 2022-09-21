302ec0700399e269a5f2d89866c2b9cccd4b326e
cactys95 <cactys95@yandex.ru>
2022-09-21 11:38:29 +0300

    fix: findBi

--- controllers/cards.js
+++ controllers/cards.js
@@ -29,7 +29,7 @@ module.exports.deleteCard = (req, res, next) => {
   const { cardId } = req.params;
   const ownerId = req.user._id;
 
-  Card.findById(cardId)
+  Card.findByIdAndRemove(cardId)
     .then((card) => {
       if (!card) {
         throw new NotFoundError('Картачка не найдена');
