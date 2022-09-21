9a051c05314cbdb4f7296c3dca796f5990f3f82e
cactys95 <cactys95@yandex.ru>
2022-09-21 12:05:59 +0300

    fix: [-] [DELETE] Удаление карточки с некорректным id

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-Joi.objectId = require('joi-objectid')(Joi);
 
 const {
   getCards,
@@ -22,19 +21,19 @@ router.post('/', celebrate({
 
 router.delete('/:cardId', celebrate({
   body: Joi.object().keys({
-    cardId: Joi.objectId(),
+    cardId: Joi.string().hex().length(24),
   }),
 }), deleteCard);
 
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.objectId(),
+    cardId: Joi.string().hex().length(24),
   }),
 }), likeCard);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.objectId(),
+    cardId: Joi.string().hex().length(24),
   }),
 }), dislikeCard);
 
