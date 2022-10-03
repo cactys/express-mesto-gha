6b77cd55cfa99f842955246016fa06d3371f0c26
cactys95 <cactys95@yandex.ru>
2022-10-03 22:07:10 +0300

    fix: allowerCors

--- utils/allowerCors.js
+++ utils/allowerCors.js
@@ -1,5 +1,8 @@
 const allowedCors = [
+  'http://khortys.nomoredomains.icu/',
+  'https://khortys.nomoredomains.icu/',
   'http://localhobdfbst:3000',
+  'https://localhobdfbst:3000',
 ];
 
 const DEFAULT_ALLOWED_METHODS = ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'];
